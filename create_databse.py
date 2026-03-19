e005ee3cc5e9e2ed103365d378dab8cb3b2c8ae3
Prathmesh Khamkar <prathmeshkhamkar19>
2026-03-20 01:08:18 +0530

    in create_database.py we create the table using create_app () and db from app and run them

---
+++ create_databse.py
@@ -0,0 +1,9 @@
+# in that file we seprately code logic to create the databse for any app
+# to create the databse we need app and the database that we mentioned 
+
+from app import create_app,db     #function and databse
+
+app = create_app()  # this create the app geting from function
+
+with app.conte(app):    #this tell we create or working on same enviorment where this class already exist
+    db.create_all()   # ead all models . convert it in table create the table in db
